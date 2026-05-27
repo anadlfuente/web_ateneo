efd9e22c7a0d9c5fdefe86ecc840bd55cf2210a8
Ana <anadlfuenteglez@gmail.com>
2026-05-27 18:38:57 +0200

    Add design to history page

--- app/historia/page.tsx
+++ app/historia/page.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
+
+
 export default function HistoriaPage() {
   return (
     <main className="min-h-screen">
@@ -19,14 +21,84 @@ export default function HistoriaPage() {
           </h1>
         </div>
       </section>
-
       {/* Contenido */}
-      <section className="max-w-5xl mx-auto px-6 py-16">
-        <h2 className="text-3xl font-serif mb-6">Quiénes somos</h2>
+      <section className="max-w-6xl mx-auto px-6 py-24 space-y-32">
 
-        <p className="text-muted-foreground leading-relaxed">
-          Aquí iría el texto de la historia del Ateneo Musical de Mieres...
-        </p>
+        {/* Bloque 1 */}
+        <div className="grid md:grid-cols-2 gap-16 items-center">
+
+          {/* Imagen */}
+          <div className="overflow-hidden rounded-2xl shadow-xl">
+            <img
+              src="/historia/first_concert.jpg"
+              alt="Historia Ateneo"
+              className="w-full h-[300px] object-cover"
+            />
+          </div>
+
+          {/* Texto */}
+          <div>
+            <h2 className="text-3xl font-serif mb-6">
+              Los comienzos
+            </h2>
+
+            <p className="text-lg leading-relaxed text-muted-foreground text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nisl eget libero pharetra lobortis ut quis libero. Vivamus lacus ligula, placerat at tristique eget, 
+              pharetra volutpat elit. Pellentesque sollicitudin ex tortor, vel pharetra felis vestibulum sed. Mauris interdum imperdiet ante. Curabitur a diam scelerisque, ultricies tellus ac, fermentum libero. Aliquam at tortor vehicula, ullamcorper enim non, consectetur sapien. Praesent feugiat tincidunt tortor, ullamcorper interdum metus ultricies quis. Etiam a ligula nec leo tincidunt ornare. Suspendisse eu sem at augue aliquam euismod. Suspendisse porta accumsan nisi, quis sodales magna finibus nec. Aliquam erat volutpat. Mauris congue eros turpis, sit amet rhoncus augue lacinia at. Proin magna lacus, feugiat in pellentesque a, posuere eu nisi. Phasellus eu ultricies elit.
+            </p>
+          </div>
+        </div>
+
+        {/* Bloque 2 */}
+        <div className="grid md:grid-cols-2 gap-16 items-center">
+
+          {/* Texto */}
+          <div>
+            <h2 className="text-3xl font-serif mb-6">
+              Creciendo juntos
+            </h2>
+
+            <p className="text-lg leading-relaxed text-muted-foreground text-justify">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nisl eget libero pharetra lobortis ut quis libero. Vivamus lacus ligula, placerat at tristique eget, 
+              pharetra volutpat elit. Pellentesque sollicitudin ex tortor, vel pharetra felis vestibulum sed. Mauris interdum imperdiet ante. Curabitur a diam scelerisque, ultricies tellus ac, fermentum libero. Aliquam at tortor vehicula, ullamcorper enim non, consectetur sapien. Praesent feugiat tincidunt tortor, ullamcorper interdum metus ultricies quis. Etiam a ligula nec leo tincidunt ornare. Suspendisse eu sem at augue aliquam euismod. Suspendisse porta accumsan nisi, quis sodales magna finibus nec. Aliquam erat volutpat. Mauris congue eros turpis, sit amet rhoncus augue lacinia at. Proin magna lacus, feugiat in pellentesque a, posuere eu nisi. Phasellus eu ultricies elit.
+            </p>
+          </div>
+
+          {/* Imagen */}
+          <div className="overflow-hidden rounded-2xl shadow-xl max-w-lg md:max-w-2xl lg:max-w-2xl mx-auto">
+            <img
+              src="/historia/BanderaAteneo.jpg"
+              alt="Historia Ateneo"
+              className="w-full h-[450px] block"
+            />
+          </div>
+        </div>
+
+        {/* Bloque 3 */}
+        <div className="grid md:grid-cols-2 gap-16 items-center">
+
+          {/* Imagen */}
+          <div className="overflow-hidden rounded-3xl shadow-xl">
+            <img
+              src="/foto_family.JPG"
+              alt="Historia Ateneo"
+              className="w-full h-[300px] object-cover"
+            />
+          </div>
+
+          {/* Texto */}
+          <div>
+            <h2 className="text-4xl font-serif mb-6">
+              El Ateneo hoy
+            </h2>
+
+            <p className="text-lg leading-relaxed text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a nisl eget libero pharetra lobortis ut quis libero. Vivamus lacus ligula, placerat at tristique eget, 
+              pharetra volutpat elit. Pellentesque sollicitudin ex tortor, vel pharetra felis vestibulum sed. Mauris interdum imperdiet ante. Curabitur a diam scelerisque, ultricies tellus ac, fermentum libero. Aliquam at tortor vehicula, ullamcorper enim non, consectetur sapien. Praesent feugiat tincidunt tortor, ullamcorper interdum metus ultricies quis. Etiam a ligula nec leo tincidunt ornare. Suspendisse eu sem at augue aliquam euismod. Suspendisse porta accumsan nisi, quis sodales magna finibus nec. Aliquam erat volutpat. Mauris congue eros turpis, sit amet rhoncus augue lacinia at. Proin magna lacus, feugiat in pellentesque a, posuere eu nisi. Phasellus eu ultricies elit.
+            </p>
+          </div>
+        </div>
+
       </section>
       <Footer />
     </main>
